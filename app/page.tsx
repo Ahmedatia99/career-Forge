dc0d0a4f7c985a6015d6bf4e499759c3b866b63e
Ahmedatia99 <atyaa629@gmail.com>
2025-12-10 16:21:41 +0200

    Feat: remove oAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react"
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
-     <Button onClick={() => signIn("github")}>Sign In</Button>
+    <div className="flex text-4xl items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      welcome to my app!
     </div>
   );
 }
