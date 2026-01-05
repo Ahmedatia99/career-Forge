3ccfc4155819ef6c5e9e8cdee5900ab0e862e75b
Ahmedatia99 <atyaa629@gmail.com>
2026-01-05 09:30:12 +0200

    feat update server

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,7 +4,7 @@ import { getToken, setToken, clearToken } from "./auth-storage";
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/v1',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
